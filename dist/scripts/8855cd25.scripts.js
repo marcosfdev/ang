f8376cb61d354e7780ed0e72f2a122efdd47b152
Marc <marcobrien12@gmail.com>
2013-05-28 16:36:58 -0700

    First commit

---
+++ dist/scripts/8855cd25.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("angularPhonecatApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("angularPhonecatApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
